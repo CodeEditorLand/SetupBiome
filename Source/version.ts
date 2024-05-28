a5611bd83f09ed4d3a868e14453c30f0657b9ffc
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-28 14:00:29 +0300

    squash!

--- Source/version.ts
+++ Source/version.ts
@@ -197,7 +197,12 @@ const fetchBiomeVersions = async (
 		);
 
 		const versions = releases
-			.filter((release) => release.tag_name.startsWith("cli/"))
+			.filter(
+				(release) =>
+					release.tag_name.startsWith("cli/") &&
+					!release.draft &&
+					!release.prerelease,
+			)
 			.map((release) => coerce(release.tag_name));
 
 		return rsort(versions as SemVer[]);
