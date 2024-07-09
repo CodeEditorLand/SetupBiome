57952e461394187044c6b495a0b8e3c75b459052
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-09 22:07:38 +0300

    squash!

--- Source/version.ts
+++ Source/version.ts
@@ -1,6 +1,6 @@
-import { existsSync } from "node:fs";
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
+import { existsSync } from "fs";
+import { readFile } from "fs/promises";
+import { join } from "path";
 import { info, warning } from "@actions/core";
 import type { Octokit } from "@octokit/rest";
 import {
